1551d19707db906aae6e0b0f0a88216075961308
Lynda Santiago <lyntechi@gmail.com>
2020-11-26 16:57:12 -0500

    created and styled skills section (#6)

---
+++ portfolio/src/components/Skills.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { skillsData } from "../data";
+
+function Skills() {
+  return (
+    <div className="skills-container">
+      <div className="grid">
+        {skillsData.map((item) => {
+          return (
+            <div>
+              {item.img}
+              <h2>{item.title}</h2>
+              <li>{item.skills.first}</li>
+              <li>{item.skills.second}</li>
+              <li>{item.skills.third}</li>
+              <li>{item.skills.fourth}</li>
+              <li>{item.skills.fifth}</li>
+              <li>{item.skills.sixth}</li>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
---
+++ portfolio/src/data.js
@@ -0,0 +1,39 @@
+import { htmlImg, designImg, serverImg } from "./svgHelpers/svgVariables";
+
+export const skillsData = [
+  {
+    img: htmlImg,
+    title: "Front End",
+    skills: {
+      first: "HTML/CSS/SASS",
+      second: "REACT",
+      third: "BOOTSTRAP/MATERIAL UI",
+      fourth: "JAVASCRIPT",
+      fifth: "REDUX",
+      sixth: "CONTEXT API",
+    },
+  },
+  {
+    img: designImg,
+    title: "Web Design",
+    skills: {
+      first: "ADOBE XD",
+      second: "FIGMA",
+      third: "WEBFLOW",
+      fourth: "ADOBE",
+      fifth: "ILLUSTRATOR",
+    },
+  },
+  {
+    img: serverImg,
+    title: "Back End",
+    skills: {
+      first: "NODE.JS",
+      second: "POSTRESSQL",
+      third: "POSTMAN",
+      fourth: "INSOMNIA",
+      fifth: "DATABASE DESIGNER",
+      sixth: "SQL LITE",
+    },
+  },
+];
