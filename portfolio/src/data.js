05adcc2fc88818ce6286668c2c7906a91cae6564
Lynda Santiago <lyntechi@gmail.com>
2020-11-26 18:31:35 -0500

    created and styled resume section

--- portfolio/src/data.js
+++ portfolio/src/data.js
@@ -37,3 +37,28 @@ export const skillsData = [
     },
   },
 ];
+
+export const resumeData = [
+  {
+    type: "Education",
+    companyName1: "Full Stack Web Development, Lambda School",
+    companyName2: "Medical Assistance Program, Lincoln Tech",
+    date1: "2020-2021",
+    date2: "2013-2014",
+    firstAbout:
+      "Lambda is a 6 to 9+ month online program that prepared me for success in web development. I was required to complete individual daily projects and I also had a chance to build real products with a team of 6-8 students from other tracks. Collaborative coding sessions and communication were held via Slack and Zoom.",
+    secondAbout:
+      "In this medical assistant program that I completed, I received training on how to take vital signs, read and update charts, apply surgical dressings, practice phlebotomy tasks like specimen collection, lab testing and etc. Some of the rooms had Doctor’s Office Classrooms that simulated a professional healthcare office, from Reception to exam rooms.",
+  },
+  {
+    type: "Experience",
+    companyName1: "Social Media Marketing Specialist, The JK Set",
+    companyName2: "Accident & Health Licensee, Optum",
+    date1: "2016-2020",
+    date2: "2015-2016",
+    firstAbout:
+      "Assisted with the general day to day administrative tasks. Monitored online media, information sources and social channels. Uploaded content on a daily basis to keep community and followers engaged. Advertised social media account on other platforms to create more brand awareness and increase followings.",
+    secondAbout:
+      "Quoted customers on available product options and accurately explain plan details. Recommended the best plans that fit the customer's needs. Exceeded monthly performance metrics. Conducted outgoing calls to provide answers and additional information to consumers. Followed a proven sales and quality process to assist customers. Data entry / updated CRM with customer data.",
+  },
+];
