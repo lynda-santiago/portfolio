1b142f48080d4ca2a6719a2b1b6eec77f43dbf1a
Lynda Santiago <lyntechi@gmail.com>
2020-11-26 01:28:22 -0500

    created header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Header from "./components//Header";
 
 function App() {
   return (
     <div className="App">
       <section className="nav-section">
         <Nav />
+      </section>
+      <section className="header-section">
+        <Header />
       </section>
     </div>
   );
---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header-container">
+    
+      <div>
+
+      </div>
+      
+      <div>
+    
+      </div>
+       
+    </div>
+  );
+}
+export default Header;
