e66d8c0ad0db45d4195d5dfe6ca2d0461b3442ea
Lynda Santiago <lyntechi@gmail.com>
2020-11-25 19:45:16 -0500

    create and styled nav (#1)

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-     
+      <section className="nav-section">
+        <Nav />
+      </section>
     </div>
   );
 }
---
+++ portfolio/src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="nav-container">
+      <div className="nav-menu">
+        <li>About</li>
+        <li>Skills</li>
+        <li>Resume</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </div>
+      <h1>Lynda Santiago</h1>
+    </div>
+  );
+}
+
+export default Nav;
