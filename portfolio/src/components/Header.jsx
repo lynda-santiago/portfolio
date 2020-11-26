a13d8adf0bcbf21f3f6016e39f2538b53ec6962b
Lynda Santiago <lyntechi@gmail.com>
2020-11-26 15:07:30 -0500

    Feature header (#4)
    * created header
    
    * finished styling header

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { heartsImg } from "../svgHelpers/svgVariables";
 
 function Header() {
   return (
     <div className="header-container">
-    
-      <div>
+      <div className="grid">
+        <div>
+          <p>My Name is Lynda Santiago</p>
+          <h3>
+            I Am A Web Developer
+            <br /> & UI Designer
+          </h3>
+          <p>This Is Truly My Passion</p>
+          <button>hire me</button>
+        </div>
 
+        <div>{heartsImg}</div>
       </div>
-      
-      <div>
-    
-      </div>
-       
     </div>
   );
 }
